ec9d987317cf9003a673384b4a6d6431a27db8bf
da kong <kongda9999@gmail.com>
2021-12-22 09:06:53 +0800

    Added Heroku postgres sequelize config

--- src/models/sequelize.ts
+++ src/models/sequelize.ts
@@ -1,16 +1,31 @@
 // @ts-ignore
 import Sequelize from 'dynamo-sequelize'
 
-const config = {
-  define: {
-    timestamps: true
-  },
-  logging: false,
-  dialect: ''
+let config;
+if (process.env.USE_HEROKU_POSTGRES) {
+  config =
+  {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  }
 }
+else {
+  config = {
+    define: {
+      timestamps: true
+    },
+    logging: false,
+    dialect: ''
+  }
 
-if (process.env.DIALECT === 'dynamodb') {
-  config.dialect = 'dynamo'
+  if (process.env.DIALECT === 'dynamodb') {
+    config.dialect = 'dynamo'
+  }
 }
 
 const sequelize = new Sequelize(
